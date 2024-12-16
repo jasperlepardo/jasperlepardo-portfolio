3cca1f67157fc6a5d7849bed89d31e5305869c66
Jasper Lepardo <jsprlprd@gmail.com>
2024-12-16 13:47:45 +0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./_site/**/*.html",  // Build output folder
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/**/*.html",
+    "./*.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
