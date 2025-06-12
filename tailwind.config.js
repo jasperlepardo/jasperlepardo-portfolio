86a0b8c1ae5e6de5d97f275d9e734bc54f5dfdd6
Jasper Lepardo <jsprlprd@gmail.com>
2025-06-13 03:25:48 +0800

    sync

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_works/**/*.html",
+    "./**/*.{md,html}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
